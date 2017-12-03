caab48b468ca74d9cce9e91d1dd58fa72464c858
Zigangirova <31883955+Zigangirova@users.noreply.github.com>
2017-12-03 18:10:34 +0300

    сделаны некоторые задачи 6 урока

--- src/lesson6/task1/Geometry.kt
+++ src/lesson6/task1/Geometry.kt
@@ -103,7 +103,23 @@ data class Segment(val begin: Point, val end: Point) {
  * Дано множество точек. Вернуть отрезок, соединяющий две наиболее удалённые из них.
  * Если в множестве менее двух точек, бросить IllegalArgumentException
  */
-fun diameter(vararg points: Point): Segment = TODO()
+fun diameter(vararg points: Point): Segment {
+    var point = Pair(0, 0)
+    var maxDistance = 0.0
+    val size = points.size
+    for (i in 0 until size) {
+        for (k in i + 1 until size) {
+            if (maxDistance < points[i].distance(points[k])) {
+                maxDistance = points[i].distance(points[k])
+                point = Pair(i, k)
+            }
+        }
+    }
+    if (points.size < 2) {
+        throw IllegalArgumentException()
+    }
+    return Segment(points[point.first], points[point.second])
+}
 
 /**
  * Простая
@@ -136,7 +152,11 @@ class Line private constructor(val b: Double, val angle: Double) {
      * Найти точку пересечения с другой линией.
      * Для этого необходимо составить и решить систему из двух уравнений (каждое для своей прямой)
      */
-    fun crossPoint(other: Line): Point = TODO()
+    fun crossPoint(other: Line): Point {
+        val x = (b * Math.cos(other.angle) - other.b * Math.cos(angle)) / Math.sin(other.angle - angle)
+        val y = (x * Math.sin(other.angle) + other.b) / Math.cos(other.angle)
+        return Point(x, y)
+    }
 
     override fun equals(other: Any?) = other is Line && angle == other.angle && b == other.b
 
@@ -154,21 +174,28 @@ class Line private constructor(val b: Double, val angle: Double) {
  *
  * Построить прямую по отрезку
  */
-fun lineBySegment(s: Segment): Line = TODO()
+fun lineBySegment(s: Segment): Line {
+    var arctg = Math.atan2((s.end.y - s.begin.y), (s.end.x - s.begin.x))
+    if (arctg < 0||arctg > Math.PI) {
+        arctg += Math.PI
+    }
+    return Line(s.begin, arctg)
+}
 
 /**
  * Средняя
  *
  * Построить прямую по двум точкам
  */
-fun lineByPoints(a: Point, b: Point): Line = TODO()
+fun lineByPoints(a: Point, b: Point): Line = lineBySegment(Segment(a, b))
 
 /**
  * Сложная
  *
  * Построить серединный перпендикуляр по отрезку или по двум точкам
  */
-fun bisectorByPoints(a: Point, b: Point): Line = TODO()
+fun bisectorByPoints(a: Point, b: Point): Line =
+        Line(Point((a.x + b.x) / 2, (a.y + b.y) / 2), (Math.PI / 2 + lineByPoints(a, b).angle) % Math.PI)
 
 /**
  * Средняя
